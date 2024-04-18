bea90558fce71611d04b7705e7ec09450f65cc9c
Hong Zhang <mahone08@gmail.com>
2024-04-17 17:40:46 -0700

    bot_v_bot with images

--- bot_v_bot.py
+++ bot_v_bot.py
@@ -6,7 +6,81 @@ from dlgo import goboard
 from dlgo import gotypes
 from dlgo.utils import print_board, print_move, clear_screen
 import time
+import pygame
+import go
 
+BACKGROUND = 'images/ramin.jpg'
+BOARD_SIZE = (820, 820)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+class Stone(go.Stone):
+    def __init__(self, board, point, color):
+        """Create, initialize and draw a stone."""
+        super(Stone, self).__init__(board, point, color)
+        self.coords = (5 + self.point[0] * 40, 5 + self.point[1] * 40)
+        self.draw()
+
+    def draw(self):
+        """Draw the stone as a circle."""
+        pygame.draw.circle(screen, self.color, self.coords, 20, 0)
+        pygame.display.update()
+
+    def remove(self):
+        """Remove the stone from board."""
+        blit_coords = (self.coords[0] - 20, self.coords[1] - 20)
+        area_rect = pygame.Rect(blit_coords, (40, 40))
+        screen.blit(background, blit_coords, area_rect)
+        pygame.display.update()
+        super(Stone, self).remove()
+
+
+class Board(go.Board):
+    def __init__(self):
+        """Create, initialize and draw an empty board."""
+        super(Board, self).__init__()
+        self.outline = pygame.Rect(45, 45, 720, 720)
+        self.draw()
+
+    def draw(self):
+        """Draw the board to the background and blit it to the screen.
+
+        The board is drawn by first drawing the outline, then the 19x19
+        grid and finally by adding hoshi to the board. All these
+        operations are done with pygame's draw functions.
+
+        This method should only be called once, when initializing the
+        board.
+
+        """
+        pygame.draw.rect(background, BLACK, self.outline, 3)
+        # Outline is inflated here for future use as a collidebox for the mouse
+        self.outline.inflate_ip(20, 20)
+        for i in range(18):
+            for j in range(18):
+                rect = pygame.Rect(45 + (40 * i), 45 + (40 * j), 40, 40)
+                pygame.draw.rect(background, BLACK, rect, 1)
+        for i in range(3):
+            for j in range(3):
+                coords = (165 + (240 * i), 165 + (240 * j))
+                pygame.draw.circle(background, BLACK, coords, 5, 0)
+        screen.blit(background, (0, 0))
+        pygame.display.update()
+
+    def update_liberties(self, added_stone=None):
+        """Updates the liberties of the entire board, group by group.
+
+        Usually a stone is added each turn. To allow killing by 'suicide',
+        all the 'old' groups should be updated before the newly added one.
+
+        """
+        for group in self.groups:
+            if added_stone:
+                if group == added_stone.group:
+                    continue
+            group.update_liberties()
+        if added_stone:
+            added_stone.group.update_liberties()
 
 def main():
     board_size = 19
@@ -16,18 +90,36 @@ def main():
         gotypes.Player.white: RandomBot(),
     }
     while not game.is_over():
-        time.sleep(0.3)  # <1>
 
-        clear_screen()   # <2>
-        print_board(game.board)
-        bot_move = bots[game.next_player].select_move(game)
-        print_move(game.next_player, bot_move)
-        game = game.apply_move(bot_move)
-        print(bot_move.point)
-        input("please enter key to continue...")
+        pygame.time.wait(250)
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                bot_move = bots[game.next_player].select_move(game)
+                game = game.apply_move(bot_move)
+                # stone = board.search(point=(bot_move.point.x, bot_move.point.y))
+
+                added_stone = Stone(board, (bot_move.point.row, bot_move.point.col), board.turn())
+                board.update_liberties(added_stone)
+            if event.type == pygame.QUIT:
+                exit()
+
+        # time.sleep(0.3)  # <1>
+
+        # clear_screen()   # <2>
+        # print_board(game.board)
+        # bot_move = bots[game.next_player].select_move(game)
+        # print_move(game.next_player, bot_move)
+        # game = game.apply_move(bot_move)
+        # print(bot_move.point)
+        # input("please enter key to continue...")
 
 
 if __name__ == '__main__':
+    pygame.init()
+    pygame.display.set_caption('Goban')
+    screen = pygame.display.set_mode(BOARD_SIZE, 0, 32)
+    background = pygame.image.load(BACKGROUND).convert()
+    board = Board()
     main()
 
 # <1> We set a sleep timer to 0.3 seconds so that bot moves aren't printed too fast to observe
